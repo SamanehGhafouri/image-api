de0f05cf54ab5f367c00edb0952473aae3aabb97
SamanehGhafouri <samaneh2010gh@gmail.com>
2024-01-03 13:24:53 -0500

    Configured media to view images on the browser, deleted uploads path

--- image_uploader/image_uploader/urls.py
+++ image_uploader/image_uploader/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/images/', include('api.urls')),
 ]
+
+# media can be viewed on the browser via their urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
